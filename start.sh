f53f124296081341238e0c51f8ca79ea329bfb2d
nknytk <nknytk.dev@gmail.com>
2014-08-05 06:56:47 +0900

    Added installation of Pillow (Python Imaging Library)

--- start.sh
+++ start.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 PYTHON="/usr/bin/python3"
+DEPENDANCIES="webcam python3-pip"
 
-WEBCAM_PKG="`dpkg -l | grep webcam | awk '{if($2=="webcam"){print $0}}'`"
-if [ -z "${WEBCAM_PKG}" ]; then
-    sudo apt-get install webcam
+for pkg in ${DEPENDANCIES}; do
+  dpkg -l ${pkg} > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    sudo apt-get install ${pkg}
+  fi
+done
+
+${PYTHON} -c "import PIL"
+if [ $? -ne 0 ]; then
+  python_ver=`${PYTHON} -V 2>&1 | cut -d" " -f2 | cut -d"." -f1,2`
+  sudo pip-${python_ver} install Pillow
 fi
 
 HRHOME=`dirname $0`
