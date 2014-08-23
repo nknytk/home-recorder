eac505ca43b288b04dad32f55f2adb3170112628
nknytk <nknytk.dev@gmail.com>
2014-08-23 19:22:55 +0900

    Devide setup script from start script.

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HRHOME=`dirname $0`
+PYTHON="/usr/bin/python3"
+DEPENDANCIES="webcam python3-pip libjpeg8-dev"
+
+for pkg in ${DEPENDANCIES}; do
+  dpkg -l ${pkg} > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    sudo apt-get install ${pkg}
+  fi
+done
+
+# for arm linux
+if [ ! -f /usr/lib/libjpeg.so ]; then
+  sudo ln -s /usr/lib/arm-linux-gnueabihf/libjpeg.so /usr/lib/
+fi
+
+${PYTHON} -c "import PIL"
+if [ $? -ne 0 ]; then
+  if [ -n "$(which pip3)" ]; then
+    python3pip=pip3
+  else
+    python_ver=`${PYTHON} -V 2>&1 | cut -d" " -f2 | cut -d"." -f1,2`
+    python3pip=pip-${python_ver}
+  fi
+  sudo ${python3pip} install Pillow
+fi
+
+echo
+echo "setup finished."
--- start.sh
+++ start.sh
@@ -2,23 +2,5 @@
 
 HRHOME=`dirname $0`
 PYTHON="/usr/bin/python3"
-DEPENDANCIES="webcam python3-pip libjpeg8-dev"
-
-for pkg in ${DEPENDANCIES}; do
-  dpkg -l ${pkg} > /dev/null 2>&1
-  if [ $? -ne 0 ]; then
-    sudo apt-get install ${pkg}
-  fi
-done
-
-if [ ! -f /usr/lib/libjpeg.so ]; then
-  sudo ln -s /usr/lib/arm-linux-gnueabihf/libjpeg.so /usr/lib/
-fi
-
-${PYTHON} -c "import PIL"
-if [ $? -ne 0 ]; then
-  python_ver=`${PYTHON} -V 2>&1 | cut -d" " -f2 | cut -d"." -f1,2`
-  sudo pip-${python_ver} install Pillow
-fi
 
 sudo ${PYTHON} ${HRHOME}/app/home-recorder.py >> ${HRHOME}/log.log 2>&1 & disown
