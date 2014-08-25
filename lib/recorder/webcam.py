4b1fb451fe12bff92cc3279bb282443d8e05fd91
nknytk <nknytk.dev@gmail.com>
2014-08-26 00:04:52 +0900

    Multi threaded camera recording for multi camera environment

--- lib/recorder/webcam.py
+++ lib/recorder/webcam.py
@@ -3,6 +3,8 @@
 import os
 import sys
 import shutil
+from time import sleep, time
+from threading import Thread
 
 from .recorderthread import  RecorderThread
 sys.path.append(os.path.join(os.path.dirname(__file__), '../util'))
@@ -15,7 +17,8 @@ class Webcam(RecorderThread):
         if os.path.exists(storage):
             shutil.rmtree(storage)
 
-        self.confs = []
+        self.confs = {}
+        camera_threads = []
         devices = self.setting['devices'] if self.setting.get('devices') else avail_cameras()
         for dev in devices:
             devname = dev.split('/')[-1]
@@ -31,14 +34,30 @@ class Webcam(RecorderThread):
 
             conf_file = os.path.join(self.datadir, devname + '.conf')
             write_webcam_config(conf, conf_file)
-            self.confs.append(conf_file)
+            self.confs[devname] = conf_file
 
-        RecorderThread.record(self, eventid, duration)
+            camera_threads.append(Thread(target=self.run_camera, args=(devname, duration)))
 
-    def snapshot(self, eventid):
-        for conf in self.confs:
-            capture(conf)
-        print('Captured image.')
+        for th in camera_threads:
+            th.start()
+        for th in camera_threads:
+            th.join()
+
+    def run_camera(self, devname, duration):
+        finishtime = time() + duration
+
+        while time() < finishtime:
+            next = time() + self.setting['interval']
+
+            capture(self.confs[devname])
+            print('Captured image with ' + devname)
+
+            if next <= finishtime:
+                now = time()
+                if next - now > 0:
+                    sleep(next - now)
+            else:
+                break
 
 
 if __name__ == '__main__':
